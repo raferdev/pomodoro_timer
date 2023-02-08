78bb14a0792834d6037b5930eba7e73c80759d13
Rafael <rafinha_fern@hotmail.com>
2023-02-07 23:40:03 -0300

    feat: overWrite defaultTheme type on typescript module

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button.js";
+import { defaultTheme } from "./styles/themes/default.js";
+
 export function App() {
 
   return (
-    <h1>Hello World!</h1>
+    <ThemeProvider theme={defaultTheme}>
+    <Button variant="success"/>
+    <Button variant="danger"/>
+    <Button variant="primary"/>
+    </ThemeProvider>
   )
 }
 
