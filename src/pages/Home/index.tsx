8d20510a05b852ca07815e2bb569604b6ec412e6
Rafael <rafinha_fern@hotmail.com>
2023-02-08 14:24:25 -0300

    feat: add react hook form as dependecies with initial setup

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -10,15 +12,24 @@ import {
 } from './styles.js'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle() {}
+
+  const task = watch('task')
+
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">{"I'm going to work in"}</label>
           <TaskInput
+            id="task"
             list="task-suggestion"
             placeholder="Give a name to the project"
-            id="task"
+            {...register('task')}
           />
           <datalist id="task-suggestion">
             <option value="adas" />
@@ -33,6 +44,7 @@ export function Home() {
             max={60}
             type="number"
             id="minutesAmount"
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <span>{'minutes.'}</span>
         </FormContainer>
@@ -43,7 +55,7 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        <StartCountdownButton type="submit">
+        <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
           <Play size={24} />
           Start
         </StartCountdownButton>
