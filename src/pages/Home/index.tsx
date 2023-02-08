f8c7b325baeac841b615eef8c93d3c7c6da9efeb
Rafael <rafinha_fern@hotmail.com>
2023-02-08 16:05:48 -0300

    feat: all values are typed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,12 +19,18 @@ const newCycleFormValidationSchema = zod.object({
   minutesAmount: zod.number().min(5).max(60),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
